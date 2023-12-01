13dccddbd2386a3de77ececda8eabe277e9a5490
Maciej Walczynski <maciej.walczynski@schibsted.pl>
2023-12-01 13:51:12 +0100

    added tests to try out pytest

---
+++ 2023/day/1/test.py
@@ -0,0 +1,22 @@
+import pytest
+from app2 import get_first_and_last
+
+
+def is_even(number):
+    return True  # Wuite buggy implementation
+
+
+@pytest.mark.parametrize(
+    "text, expected",
+    [
+        ("tpxrcthqbktwoeightonepcc4tzf26", 26),
+        ("659nfbpb", 69),
+        ("oneone2", 12),
+        ("tmppzkhk49nine4", 44),
+        ("nineight", 98),
+        ("one", 11),
+        ("threeeightfive6fxlckcsskpnd", 36),
+    ],
+)
+def test_get_first_and_last(text, expected):
+    assert get_first_and_last(text) == expected
